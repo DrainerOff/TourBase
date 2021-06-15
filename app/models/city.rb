164969576b6740fab45e24a62feed5a98caec9fa
DrainerOff <vladluzenko@gmail.com>
2021-06-15 10:07:09 +0300

    Мелкие визуальные улучшения, и удаление ненужных классов

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,6 @@
 class City < ApplicationRecord
   has_many :tourbases
+
   belongs_to :region
   validates :city_name, presence: true, length: {minimum: 1}
 end
--- app/models/tourbase.rb
+++ app/models/tourbase.rb
@@ -1,4 +1,4 @@
 class Tourbase < ApplicationRecord
   belongs_to :city
-  validates :tourbase_name, presence: true, length: {minimum: 1}
+  validates :tourbase_name, presence: true, length: {maximum: 20}
 end
