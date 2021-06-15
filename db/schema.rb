84bef05f6392fd78e7b70b009536558663a25386
DrainerOff <vladluzenko@gmail.com>
2021-06-15 14:49:20 +0300

    Перенастроил проект, для успешной загрузки на heroku

--- db/schema.rb
+++ db/schema.rb
@@ -15,24 +15,26 @@ ActiveRecord::Schema.define(version: 2021_06_14_124416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cities", primary_key: "city_id", id: :integer, default: nil, force: :cascade do |t|
+  create_table "cities", primary_key: "city_id", id: :integer, auto_increment: true, default: nil, force: :cascade do |t|
     t.string "city_name", null: false
     t.integer "region_id", null: false
   end
 
-  create_table "countries", primary_key: "country_id", id: :integer, default: nil, force: :cascade do |t|
+  create_table "countries", primary_key: "country_id", id: :integer,  auto_increment: true, default: nil, force: :cascade do |t|
     t.string "country_name", null: false
   end
 
-  create_table "regions", primary_key: "region_id", id: :integer, default: nil, force: :cascade do |t|
+  create_table "regions", primary_key: "region_id", id: :integer,  auto_increment: true, default: nil, force: :cascade do |t|
     t.string "region_name", null: false
     t.integer "country_id", null: false
   end
 
-  create_table "tourbases", force: :cascade do |t|
-    t.string "name"
+  create_table "tourbases", primary_key: "tourbase_id", id: :integer,  auto_increment: true, default: nil, force: :cascade do |t|
+    t.string "tourbase_name", null: false
+    t.integer "city_id", null: false
   end
 
   add_foreign_key "cities", "regions", primary_key: "region_id", name: "cities_region_id_fkey"
   add_foreign_key "regions", "countries", primary_key: "country_id", name: "regions_country_id_fkey"
+  add_foreign_key "tourbases", "cities", primary_key: "city_id", name: "tourbases_city_id_fkey"
 end
